38d22dab23463e93cc9e1b7941287787150919a9
tonkla <tonkla.p2004@gmail.com>
2025-09-06 19:21:19 +0700

    config url

--- next.config.ts
+++
@@ -1,5 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/https://github.com/Menthhh/tonkla-portfolio-site/' : '',
-};
