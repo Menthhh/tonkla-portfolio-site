81f24f20549dd794c5752e64c4ee0f290c4b0dee
tonkla <tonkla.p2004@gmail.com>
2025-09-06 19:34:47 +0700

    config url path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tonkla-portfolio-site/" : "/",
+  base: "/tonkla-portfolio-site/",
   server: {
     host: "::",
     port: 8080,
